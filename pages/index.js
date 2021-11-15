930fa6afa20c60349e03e29124599ca7d0449878
Dinesh Tamang <dineshtamang7263@gmail.com>
2021-11-15 16:48:02 +0530

    added server side rendering

--- pages/index.js
+++ pages/index.js
@@ -15,6 +15,28 @@ export default function Home() {
       {/* Banner */}
       <Banner />
 
+      <main className="max-w-7xl mx-auto px-8 sm:px-16">
+        <section className="pt-6">
+          <h2 className="text-4xl font-semibold pb-5">Explore Nearby</h2>
+
+          {/* pull some data from a server */}
+
+        </section>
+      </main>
+
     </div>
   )
+}
+
+export async function getStaticProps(){
+  const exploreData = await fetch("https://links.papareact.com/pyp")
+  .then(
+    (res) => res.json()
+  );
+
+  return {
+    props: {
+      exploreData
+    }
+  }
 }
