95dafacca73cfa2117689e52f01972d26970c11f
Dinesh Tamang <dineshtamang7263@gmail.com>
2021-11-16 16:20:37 +0530

    added footer components

--- pages/index.js
+++ pages/index.js
@@ -4,6 +4,7 @@ import Banner from '../components/Banner'
 import SmallCard from '../components/SmallCard';
 import MediumCard from '../components/MediumCard';
 import LargeCard from '../components/LargeCard';
+import Footer from '../components/Footer';
 
 export default function Home({ exploreData, cardsData }) {
   return (
@@ -53,6 +54,9 @@ export default function Home({ exploreData, cardsData }) {
         />
 
       </main>
+
+      <Footer />
+
     </div>
   );
 }
