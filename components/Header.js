d9b6f2d2eeb1f2c1af5bb736973aea0451670825
Dinesh Tamang <dineshtamang7263@gmail.com>
2021-11-17 17:13:03 +0530

    added calender feature

--- components/Header.js
+++ components/Header.js
@@ -15,6 +15,7 @@ function Header() {
     const [searchInput, setSearchInput] = useState("");
     const [startDate, setStartDate] = useState(new Date());
     const [endDate, setEndDate] = useState(new Date());
+    const [noOfGuests, setNoOfGuests] = useState(1);
 
     const handleSelect = (ranges) =>{
         setStartDate(ranges.selection.startDate)
@@ -27,47 +28,69 @@ function Header() {
         key: "selection"
     }
 
+    const resetInput = () => {
+        setSearchInput('');
+    }
+
     return (
-        <header className="sticky top-0 z-50 grid grid-cols-3 bg-white shadow-md p-5 md:px-10">
-           {/* Left */}
-           <div className="relative flex items-center h-10 cursor-pointer my-auto">
-               <Image 
-                   src="https://links.papareact.com/qd3"
-                   layout="fill"
-                   objectFit="contain"
-                   objectPosition="left"
-               />
-           </div>
+      <header className="sticky top-0 z-50 grid grid-cols-3 bg-white shadow-md p-5 md:px-10">
+        {/* Left */}
+        <div className="relative flex items-center h-10 cursor-pointer my-auto">
+          <Image
+            src="https://links.papareact.com/qd3"
+            layout="fill"
+            objectFit="contain"
+            objectPosition="left"
+          />
+        </div>
 
-           {/* Middle - search */}
-           <div className="flex items-center md:border-2 rounded-full py-2 md:shadow-sm">
-               <input value={searchInput} onChange={e => setSearchInput(e.target.value)} className="flex-grow pl-5 bg-transparent outline-none text-sm text-gray-600 placeholder-gray-400" type="text" placeholder="Start your search" />
-               <SearchIcon className="hidden md:inline-flex h-8 bg-red-400 text-white rounded-full p-2 cursor-pointer md:mx-2" />
-           </div>
+        {/* Middle - search */}
+        <div className="flex items-center md:border-2 rounded-full py-2 md:shadow-sm">
+          <input
+            value={searchInput}
+            onChange={(e) => setSearchInput(e.target.value)}
+            className="flex-grow pl-5 bg-transparent outline-none text-sm text-gray-600 placeholder-gray-400"
+            type="text"
+            placeholder="Start your search"
+          />
+          <SearchIcon className="hidden md:inline-flex h-8 bg-red-400 text-white rounded-full p-2 cursor-pointer md:mx-2" />
+        </div>
 
-           {/* Right */}
-           <div className="flex items-center space-x-4 justify-end cursor-pointer text-gray-500">
-               <p className="hidden md:inline">Become a Host</p>
-               <GlobeAltIcon className="h-6" />
-               <div className="flex items-center space-x-2 border-2 p-2 rounded-full">
-                   <MenuIcon className="h-6" />
-                   <UserCircleIcon className="h-6" />
-               </div>
-           </div>
+        {/* Right */}
+        <div className="flex items-center space-x-4 justify-end cursor-pointer text-gray-500">
+          <p className="hidden md:inline">Become a Host</p>
+          <GlobeAltIcon className="h-6" />
+          <div className="flex items-center space-x-2 border-2 p-2 rounded-full">
+            <MenuIcon className="h-6" />
+            <UserCircleIcon className="h-6" />
+          </div>
+        </div>
 
-           {searchInput && (
-               <div>
-                    <DateRangePicker 
-                        ranges={[selectionRange]}
-                        minDate={new Date()}
-                        rangeColors={["#FD5B61"]}
-                        onChange={handleSelect}
-                    />
-               </div>
-           )}
+        {searchInput && (
+          <div className="flex flex-col col-span-3 mx-auto">
+            <DateRangePicker
+              ranges={[selectionRange]}
+              minDate={new Date()}
+              rangeColors={["#FD5B61"]}
+              onChange={handleSelect}
+            />
 
-        </header>
-    )
+            <div className="flex items-center border-b mb-4">
+              <h2 className="text-2xl flex-grow font-semibold">
+                Number of Guests
+              </h2>
+              <UserIcon className="h-5" />
+              <input value={noOfGuests} min={1} onChange={(e) => setNoOfGuests(e.target.value)} className="w-12 pl-2 text-lg outline-none text-red-400" type="number" />
+            </div>
+
+            <div className="flex">
+                <button onClick={resetInput} className="flex-grow text-gray-500">Cancel</button>
+                <button className="flex-grow text-red-400">Search</button>
+            </div>
+          </div>
+        )}
+      </header>
+    );
 }
 
 export default Header;
