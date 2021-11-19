5c6c8fc5a4b1bc04099b0707808d36336a03bbbb
Dinesh Tamang <dineshtamang7263@gmail.com>
2021-11-19 16:40:36 +0530

    added map feature

--- components/Map.js
+++ components/Map.js
@@ -1,8 +1,23 @@
+import { useState } from "react";
 import ReactMapGL from "react-map-gl";
 
 function Map() {
+  const [viewport, setViewPort] = useState({
+    width: "100%",
+    height: "100%",
+    latitude: 37.7577,
+    longitude: -122.4376, 
+    zoom: 11,
+  });
+
     return (
-      <ReactMapGL mapStyle="mapbox://styles/dineshtamang/ckw65yq3f5b3p15o61mcgqz88"></ReactMapGL>
+      <ReactMapGL
+       mapStyle="mapbox://styles/dineshtamang/ckw65yq3f5b3p15o61mcgqz88" 
+       mapboxApiAccessToken={process.env.mapbox_key}
+       {...viewport}
+       >
+
+       </ReactMapGL>
     );
 }
 
