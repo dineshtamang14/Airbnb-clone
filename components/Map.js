e311924b26c26d67a287753d007a2087f5047fea
Dinesh Tamang <dineshtamang7263@gmail.com>
2021-11-19 14:57:35 +0530

    added mapbox

---
+++ components/Map.js
@@ -0,0 +1,9 @@
+import ReactMapGL from "react-map-gl";
+
+function Map() {
+    return (
+      <ReactMapGL mapStyle="mapbox://styles/dineshtamang/ckw65yq3f5b3p15o61mcgqz88"></ReactMapGL>
+    );
+}
+
+export default Map;
--- next.config.js
+++ next.config.js
@@ -1,5 +1,9 @@
 module.exports = {
-    images: {
-        domains: ["links.papareact.com"],
-    },
-}
+  images: {
+    domains: ["links.papareact.com"],
+  },
+  env: {
+    mapbox_key:
+      "pk.eyJ1IjoiZGluZXNodGFtYW5nIiwiYSI6ImNrdzY1cndveDBrdXQybnJoa2NscmswdDIifQ.-ussuwVMhhzBGZ1tipZ0Ow",
+  },
+};
